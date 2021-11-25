b451495d0384f91cb201be6b62909d15621efb5e
DongJo Kim <dong.jo.kim@cern.ch>
2021-11-25 20:44:46 +0200

    remove pPb 1-2GeV, errors are too large

--- Fig6_vn_allSystems_comp.py
+++ Fig6_vn_allSystems_comp.py
@@ -16,7 +16,7 @@ data = {
 	"vn_pp_14":ROOT.TFile("data/Final_Items.root","read"), # pp 1<pT<4 GeV
 #	"vn_pPb_zna":ROOT.TFile("data/Final_Items.root","read"),
 #	"vn_pPb_v0a":ROOT.TFile("data/Final_Items.root","read"),
-	"vn_pPb_v0a":ROOT.TFile("data/fout_v2_pPb_v1.root","read"),  # pPb 1<pT<2 GeV
+#	"vn_pPb_v0a":ROOT.TFile("data/fout_v2_pPb_v1.root","read"),  # pPb 1<pT<2 GeV
 	"vn_pPb_v0a_14":ROOT.TFile("data/fout_v2_pPb_v1.root","read"),  # pPb 1<pT<4 GeV
 #	"vn_pp_pub":ROOT.TFile("../flow_in_small_and_large_systems/Data/output_vn_pp.root","read"),
 #	"vn_pPb_pub":ROOT.TFile("../flow_in_small_and_large_systems/Data/output_vn_pPb.root","read"),
@@ -26,25 +26,25 @@ data = {
 }
 
 plotParams = {
-	"vn_pp":{"color":"k","fmt":"o","markersize":5.0,"label":"pp $\\sqrt{s}$ = 13 TeV ","labelLegendId":0},
-	"vn_pp_14":{"color":"r","fmt":"o","mfc":"none","markersize":5.0,"label":"pp ","labelLegendId":1},
+	"vn_pp":{"color":"k","fmt":"o","markersize":5.0,"label":"pp 13 TeV, $1 < p_\\mathrm{T} < 2.0 \\,\\mathrm{GeV}/c$","labelLegendId":0},
+	"vn_pp_14":{"color":"r","fmt":"o","mfc":"none","markersize":5.0,"label":"pp 13 TeV, $1 < p_\\mathrm{T} < 4.0 \\,\\mathrm{GeV}/c$","labelLegendId":0},
 #	"vn_pPb_zna":{"color":"b","fmt":"s","mfc":"none","markersize":5.0,"label":"2PC: pPb $\\sqrt{s_\\mathrm{NN}}$ = 5.02 TeV, ZNA","labelLegendId":0},
 #	"vn_pPb_v0a":{"color":"b","fmt":"s","mfc":"none","markersize":5.0,"label":"2PC: pPb $\\sqrt{s_\\mathrm{NN}}$ = 5.02 TeV, V0A","labelLegendId":0},
-	"vn_pPb_v0a":{"color":"k","fmt":"*","markersize":5.0,"label":"p$-$Pb $\\sqrt{s_\\mathrm{NN}}$ = 5.02 TeV ","labelLegendId":0},
-	"vn_pPb_v0a_14":{"color":"r","fmt":"*","mfc":"none","markersize":5.0,"label":"p$-$Pb ","labelLegendId":1},
+#	"vn_pPb_v0a":{"color":"k","fmt":"*","markersize":5.0,"label":"p$-$Pb $\\sqrt{s_\\mathrm{NN}}$ = 5.02 TeV ","labelLegendId":0},
+	"vn_pPb_v0a_14":{"color":"r","fmt":"D","mfc":"none","markersize":5.0,"label":"p$-$Pb 5.02 TeV, $1 < p_\\mathrm{T} < 4.0 \\,\\mathrm{GeV}/c$","labelLegendId":0},
 #	"vn_pPb_pub":{"color":"orange","fmt":"p","mfc":"none","markersize":5.0,"label":"$V_{2}\\{2,|\Delta\eta|<1.4\\}$: pPb $\\sqrt{s_\\mathrm{NN}}$ = 5.02 TeV","labelLegendId":1},
 	#"vn_PbPb_pub":{"color":"brown","fmt":"D","mfc":"none","markersize":5.0,"label":"$V_{2}\\{2\\}$: Pb$-$Pb $\\sqrt{s_\\mathrm{NN}}$ = 5.02 TeV","labelLegendId":1},
 	#"vn_pp_atl":{"color":"green","fmt":"o","markersize":5.0,"label":"ATLAS: pp $\\sqrt{s}$ = 13 TeV","labelLegendId":2},
 	#"vn_pPb_atl":{"color":"purple","fmt":"s","mfc":"none","markersize":5.0,"label":"ATLAS: p$-$Pb $\\sqrt{s_\\mathrm{NN}}$ = 5.02 TeV","labelLegendId":2},
 	"vn_pp_pseudo":{"color":"pink","fmt":"o","mfc":"none","markersize":5.0,"label":"pp 1-4 GeV ","labelLegendId":0},
-	"vn_pPb_v0a_pseudo":{"color":"blue","fmt":"*","mfc":"none","markersize":5.0,"label":"p$-$Pb 1-4 GeV","labelLegendId":0},
+#	"vn_pPb_v0a_pseudo":{"color":"blue","fmt":"*","mfc":"none","markersize":5.0,"label":"p$-$Pb 1-4 GeV","labelLegendId":0},
 	"ptBin1":{"color":"black",}
 
 
 }	
 #Histogran names corresponding to system and experiment
 #histNames = ["pp","pPb_v2_zna","pPb_v2_v0a"];
-histNames = ["pp","pp_14","pPb_v2_v0a","pPb_14"];
+histNames = ["pp","pp_14","pPb_14"];
 #histNamesPub = ["ppHM","pPb","PbPb"];
 histNamesAtlas = ["pp13","pPb"];
 NremoveAtlas =[3,5]
@@ -54,7 +54,7 @@ nrow = 1;
 ncol = 1;
 xlimits = [(0,100),(0,100)];
 ylimits = [(-0.005,0.13)];
-rlimits = [(0.5,1.5),(0.,4.5)];
+rlimits = [(0.5,1.6),(0.,4.5)];
 
 #Alice to atlas conversion on multiplicity = N_{ch}
 NchAtlaspp_pythiaOnly = [80.33, 48.83, 29.15, 14.12];
@@ -90,7 +90,7 @@ plot = JPyPlotRatio.JPyPlotRatio(panels=(nrow,ncol),
 	panelLabelLoc=(0.85,0.85),panelLabelSize=16,panelLabelAlign="left",
 	legendPanel={0:0,1:0,2:0},
 	#legendPanel={0:0},
-        legendLoc={0:(0.45,0.16),1:(0.75,0.16),2:(0.75,0.12)},
+        legendLoc={0:(0.65,0.26),1:(0.75,0.16),2:(0.75,0.12)},
        	#legendLoc={0:(0.60,0.15)},
 	legendSize=7,xlabel=xtitle[0],ylabel=ytitle[0]);
 
@@ -98,7 +98,7 @@ plot = JPyPlotRatio.JPyPlotRatio(panels=(nrow,ncol),
 
 plotMatrix = np.empty((nrow,ncol),dtype=int);
 
-plot.GetAxes(0).set_xticks([0,20,40,60,80,120,150,200,250,300]);
+plot.GetAxes(0).set_xticks([0,10,20,30,40,50,60,70,100, 120, 150, 200, 250,300]);
 #plot.GetAxes(1).set_xticks([0,15, 30, 45,60]);
 plot.GetAxes(0).xaxis.set_ticks_position('both');
 plot.GetAxes(0).yaxis.set_ticks_position('both');
@@ -137,7 +137,7 @@ for i,s in enumerate(data):
 	plots[s] = plot.Add(0,(x,y,yerr),**plotParams[s]); # to replace with ATLAS converted Nch
 	plot.AddSyst(plots[s],grsyst);
 plot.Ratio(plots["vn_pp_14"], plots["vn_pp"]);
-plot.Ratio(plots["vn_pPb_v0a_14"], plots["vn_pPb_v0a"]);
+#plot.Ratio(plots["vn_pPb_v0a_14"], plots["vn_pPb_v0a"]);
 
 
 
@@ -145,9 +145,9 @@ plot.Ratio(plots["vn_pPb_v0a_14"], plots["vn_pPb_v0a"]);
 
 
 plot.GetPlot().text(0.19,0.8,"ALICE",fontsize=14);
-plot.GetPlot().text(0.5,0.35,"$1.6<|\Delta\eta|<1.8$",fontsize=8);
+plot.GetPlot().text(0.5,0.55,"$1.6<|\Delta\eta|<1.8$",fontsize=8);
 #plot.GetPlot().text(0.35,0.3,"$1 < p_\\mathrm{T} < 2.0 \\,\\mathrm{GeV}/c$",fontsize=8);
-plot.GetPlot().text(0.65,0.3,"$1 < p_\\mathrm{T} < 4.0 \\,\\mathrm{GeV}/c$",fontsize=8);
+#plot.GetPlot().text(0.65,0.3,"$1 < p_\\mathrm{T} < 4.0 \\,\\mathrm{GeV}/c$",fontsize=8);
 #plot.GetPlot().text(0.55,0.53,"$0.2 < p_\\mathrm{T} < 3.0 \\,\\mathrm{GeV}/c$",fontsize=8);
 #plot.GetPlot().text(0.55,0.65,"$|\Delta\eta|<1.4$",fontsize=8);
 #plot.GetPlot().text(0.5,0.36,"$2.0<|\Delta\eta|<5.0$",fontsize=8);
