241e084a0df47b7b2bfca897a090f22bcedd3706
DongJo Kim <dong.jo.kim@cern.ch>
2023-04-10 11:39:49 +0300

    update

--- Fig2_vn_ppPb.py
+++ Fig2_vn_ppPb.py
@@ -17,8 +17,8 @@ f = ROOT.TFile("data/Final_Items.root","read");
 fpPb = ROOT.TFile("data/fout_v2_pPb_v2.root","read");
 dataTypePlotParams = [
 	{'plotType':'data','color':'r','fmt':'o','markersize':5.0},
-	{'plotType':'data','color':'g','fmt':'d','markersize':5.0},
-	{'plotType':'data','color':'b','fmt':'s','markersize':5.0},
+	{'plotType':'data','color':'g','fmt':'d','fillstyle':'none','markersize':5.0},
+	{'plotType':'data','color':'b','fmt':'s','fillstyle':'none','markersize':5.0},
 	{'plotType':'theory','facecolor':'C0','edgecolor':'C0','alpha':0.5,'linestyle':'solid','linecolor':'C0'},
 	{'plotType':'theory','facecolor':'C1','edgecolor':'C1','alpha':0.5,'linestyle':'dotted','linecolor':'C1'},
 	{'plotType':'theory','facecolor':'C2','edgecolor':'C2','alpha':0.5,'linestyle':'dashed','linecolor':'C2'},
